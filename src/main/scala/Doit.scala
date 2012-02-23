ccf92ce3a559c2d55b62a17952de6f4ac8367159
Vincent Jousse <vincent@jousse.org>
2012-02-23 23:19:49 +0100

    Remove signal handling

--- src/main/scala/Doit.scala
+++ src/main/scala/Doit.scala
@@ -3,8 +3,6 @@ package doit
 import akka.actor._
 import akka.util.duration._
 
-import scala.tools.util.SignalManager
-
 object Doit extends App {
 
   val system = ActorSystem("DoitSystem")
@@ -24,13 +22,4 @@ object Doit extends App {
     10 seconds,
     tickActor,
     PoisonPill)
-
-
-  Seq("TERM", "INT") map { sig =>
-    SignalManager(sig) = {
-      env.logger("[SIG%s] Asking the reaper to terminate nicely..." format sig)
-      self ! PoisonPill
-    }
-  }
-
 }
