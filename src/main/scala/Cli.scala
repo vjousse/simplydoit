543f3aa5b1951d6311e987b38c60b1824c53aa09
Vincent Jousse <vincent@jousse.org>
2012-03-16 16:45:39 +0100

    Add basic Cli object

---
+++ src/main/scala/Cli.scala
@@ -0,0 +1,24 @@
+package doit
+
+object Cli {
+
+  def main(args: Array[String]): Unit = sys exit {
+    println(args.toList match {
+      case config :: command :: args ⇒ {
+        val env = Env(config)
+        EnvCli(env)(command, args)
+      }
+      case _ ⇒ "Usage: run /path/to/simplydoit.conf command args"
+    })
+    0
+  }
+
+  case class EnvCli(env: Env) {
+
+    def apply(command: String, args: List[String]) = command match {
+      case command  ⇒ "Unknown command " + command
+    }
+
+  }
+
+}
